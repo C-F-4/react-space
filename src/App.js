d1a126bcdabf61fed2c5a19cd7239696b67b8e45
C-F-4 <abhinavsaxena67@gmail.com>
2022-03-20 19:49:06 +0530

    Test JSON DB

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React, { useState, useEffect } from 'react';
 import './style.scss';
 import Tasks from './screens/Tasks/Tasks';
 import { uuid } from './utils/lib';
-import { Tasklist } from './mock-data/tasks';
+// import { Tasklist } from './mock-data/tasks';
 
 export default function App() {
-  const [tasks, setTasks] = useState(Tasklist || []);
+  const [tasks, setTasks] = useState([]);
+
+  const apiBase = `https://json-server-dwgyal--3000.local.webcontainer.io/TM-hxmxu9`;
+  const tasksBase = `-tasks`;
+  const isServerUp = true;
 
   useEffect(() => {
-    console.log(tasks);
-  }, [tasks]);
+    // Test Loading Tasks
+    const fetchTasks = async () => {
+      const res = await fetch(`${apiBase}${tasksBase}`);
+      const data = res.json();
+      console.log(res, data);
+    };
+    if (isServerUp) {
+      fetchTasks();
+    }
+  }, []);
 
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
