094a0b1db75da04fc98730316a5c56d9edbcaf74
C-F-4 <abhinavsaxena67@gmail.com>
2022-03-20 10:46:52 +0530

    Lift State up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.scss';
 import Tasks from './screens/Tasks/Tasks';
 import { Tasklist } from './mock-data/tasks';
 
 export default function App() {
+  const [tasks, setTasks] = useState(Tasklist);
+
   const deleteTask = (id) => {
-    console.debug(`Deleting Task ${id}`);
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const addTask = () => {
+    setTasks([
+      ...tasks,
+      {
+        id: `tid${tasks.length}`,
+        title: `Task 0${tasks.length + 1}`,
+        date: 'Feb 19th at 12:30pm',
+      },
+    ]);
   };
 
   return (
     <div className="app">
-      <Tasks tasklist={Tasklist} onDelete={deleteTask}></Tasks>
+      <Tasks tasklist={tasks} onAdd={addTask} onDelete={deleteTask}></Tasks>
     </div>
   );
 }
--- src/screens/Tasks/Tasks.js
+++ src/screens/Tasks/Tasks.js
@@ -5,30 +5,34 @@ import Header from './../../components/Header/Header';
 import Button from './../../components/Button/Button';
 import Task from './../../components/Task/Task';
 
-const Tasks = ({ tasklist, onDelete } = props) => {
-  const [tasks, setTasks] = useState(tasklist);
+const Tasks = ({ tasklist, onAdd, onDelete } = props) => {
+  // const [tasks, setTasks] = useState(tasklist);
+  const tasks = tasklist;
 
   const handleAdd = () => {
-    console.log('Add');
+    onAdd();
   };
 
   return (
     <>
       <FrameBorder classlist={'p-20'}>
-        <Header title="Task Manager">
+        <Header title="Task Manager" classlist={'mb-20'}>
           <Button text="Add" classlist={'btn-primary'} onClick={handleAdd} />
         </Header>
-        <p>Start adding tasks to see some magic happen :)</p>
-        {tasks?.map((task) => (
-          <Task
-            id={task.id}
-            title={task.title}
-            date={task.date}
-            key={task.id}
-            classlist={'mb-10'}
-            onDelete={onDelete}
-          ></Task>
-        ))}
+        {tasks?.length ? (
+          tasks?.map((task) => (
+            <Task
+              id={task.id}
+              title={task.title}
+              date={task.date}
+              key={task.id}
+              classlist={'mb-10'}
+              onDelete={onDelete}
+            ></Task>
+          ))
+        ) : (
+          <p>Start adding tasks to see some magic happen :)</p>
+        )}
       </FrameBorder>
     </>
   );
